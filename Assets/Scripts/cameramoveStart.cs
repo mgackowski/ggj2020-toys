4bb72e35719bf8b7fecf9170a3c9f2feb598c59e
overoli <47273837+overoli@users.noreply.github.com>
2021-01-30 17:12:19 +0000

    myscene
    small animation, ui canvas

---
+++ Assets/Scripts/cameramoveStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramoveStart : MonoBehaviour
+{
+
+    public Animator anim;
+
+
+
+    public void movecam()
+    {
+
+        anim.Play("MoveCam");
+    }
+}
