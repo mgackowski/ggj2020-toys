3d285d745b93d9c98fbe9b48323cf1a6f9c005cf
mgackowski <mwgackowski@gmail.com>
2021-01-30 14:24:22 +0000

    Simple hook movement scene

---
+++ Assets/Scripts/AlignYWithObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignYWithObject : MonoBehaviour
+{
+    public GameObject target;
+    public float yOffset = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y = target.transform.position.y + yOffset;
+        transform.position = newPosition;
+    }
+}
